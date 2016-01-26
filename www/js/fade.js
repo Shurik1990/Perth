033a5b88ad78c60f2e583ffac87064530c3764d7
Alexander Shyshkov <shisha_90@list.ru>
2016-01-26 20:40:41 +0200

    first commit

---
+++ www/js/fade.js
@@ -0,0 +1,8 @@
+$('#menu').click(function(){
+            $('#hide').fadeIn(700);
+               return false;
+            });
+$('#menu-hide').click(function(){
+            $('#hide').fadeOut(700);
+               return false;
+            });
